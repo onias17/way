a9602c107744e087b1daab1f2d9344624442f933
onias17 <oniasnephiisrael@gmail.com>
2021-01-11 00:17:13 -0800

    styles

--- main_app/forms.py
+++ main_app/forms.py
@@ -8,20 +8,16 @@ class ProfileCreationForm(forms.ModelForm):
         fields = [ 'email', 'slug', 'firstname', 'lastname', 'currentcity', 'picture' ]
         widgets = {'slug' : forms.HiddenInput()}
 
-
 class PostCreationForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'picture']
-
-
 
 class CityCreationForm(forms.ModelForm):
     class Meta:
         model = City
         fields = ['name', 'slug', 'picture', 'country']
         widgets = { 'slug' : forms.HiddenInput()}
-
 
 class CommentCreationForm(forms.ModelForm):
     class Meta:
